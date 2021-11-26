d6137045bf0e3e05d19bd98e3fd43fbe7c90fb9e
AbulHossain/Adnan <alhossain.a92@gmail.com>
2021-11-26 08:28:56 -0800

    encapsulations

--- accessmodifire.php
+++ accessmodifire.php
@@ -25,4 +25,28 @@ echo $obj->age();
 ?>
 <p>private modifire:only can access inside class</p>
 <p>protected can access inside class and chaild class</p>
-<p>public modifire can access form anyware</p>
+<p>public modifire can access form anywere</p>
+
+
+
+<?php 
+
+class student2{
+	private $num;
+	
+	function __construct(){
+		 $this->num=1;
+	}
+	function test(){
+		echo $this->num;
+	}
+	
+}
+
+
+$obj=new student2();
+
+echo $obj->test();
+
+?>
+<p> ayvabe function call set kore privet ba protected value retrive kora jai</p>
