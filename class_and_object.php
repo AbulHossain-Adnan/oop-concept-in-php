4836503e76cf2b0db914e36f2177c801bc2c6281
AbulHossain/Adnan <alhossain.a92@gmail.com>
2021-11-26 06:20:19 -0800

    class and object updated

--- class_and_object.php
+++ class_and_object.php
@@ -37,4 +37,19 @@ echo "<br>";
 
 $obj->set_color('red');
 echo "color is:"." ". $obj->get_color();
+?><br>
+
+<?php 
+class tree{
+	public $x=2;
+	function func1(){
+		return $this->x++;
+	}
+}
+$obj=new tree();
+echo $obj->x;
+echo "<br>";
+$obj2=new tree();
+$obj->func1();
+echo $obj->x;
 ?>
