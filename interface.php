80edeefe57c0dc3ca76dedd6abbdff22d1ea27dc
AbulHossain/Adnan <alhossain.a92@gmail.com>
2021-11-26 10:18:12 -0800

    interface

---
+++ interface.php
@@ -0,0 +1,4 @@
+interface its work like inheritance but inheritance not support multiple inherit.
+2.no constructor in interface.
+3.interface support multiple inheritance.
+4.all fuctions must be public.
