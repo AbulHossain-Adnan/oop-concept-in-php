2649d01c38104dd7d7ee108693d50b06d2547808
AbulHossain/Adnan <alhossain.a92@gmail.com>
2021-11-26 09:25:54 -0800

    abstract class

---
+++ abstractclass.php
@@ -0,0 +1,5 @@
+1.abstract class contain atlast 1 abstract function.
+2.abstract function must declear but not implement.
+3.abstract could not create object.
+4.abstract class ,child class must contain abstract function.
+
