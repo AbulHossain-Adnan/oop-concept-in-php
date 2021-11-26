4fafe4ddf9bd56b52d78d41843b2ddf829e851dc
AbulHossain/Adnan <alhossain.a92@gmail.com>
2021-11-26 06:54:35 -0800

    con destruct update

--- constructor_destructor.php
+++ constructor_destructor.php
@@ -18,4 +18,20 @@ $obj=new frute();
 echo "<br>";
 echo $obj->test();
 ?>
-<p> when creat  object of class  then by default contructor will run and destructor will run end of the script</p>
+<p> when creat  object of class  then by default contructor will run and destructor will run end of the script</p><br>
+<h1>my name is adnan</h1>
+
+<?php 
+class test{
+	function __construct($y){
+		$this->x=$y;
+
+	}
+	function func(){
+		echo $this->x;
+	}
+}
+$object=new test(20);
+echo $object->func();
+?><br>
+
