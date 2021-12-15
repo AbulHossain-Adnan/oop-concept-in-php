1da5bc65028f8ef831ea8616d30dd88c97bb10c3
adnan <alhossain.a92@gmail.com>
2021-12-14 20:00:17 -0800

    abstract practice

--- abstractclass.php
+++ abstractclass.php
@@ -1,11 +1,36 @@
-1.abstract class contain atlast 1 abstract function.
+<!-- 1.abstract class contain atlast 1 abstract function.
 2.abstract function must declear but not implement.
 3.abstract could not create object.
-4.abstract class ,child class must contain abstract function.
+4.abstract class ,child class must contain abstract function. -->
 
 <?php
 abstract class visha{
-	function show();
-} 
+	private function n(){
+		echo "bye";
+	}
+
+	 abstract public function show();
+
+
+	}
+
+	class ticket extends visha{
+
+		public function show(){
+			echo "hello";
+		}
+		public function n(){
+			echo "dfdfd";
+		}
+	}
+
+	$obj =new ticket();
+	$obj->show();
+	echo "<br>";
+	$obj->n();
+
+
+
+
 ?>
 
